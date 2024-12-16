c0fc9533107f1bd64959abe09a2b5ce4e71f0b37
Rafassz <rafassz.aps@gmail.com>
2024-12-16 10:16:25 -0300

    CRUD Categoria testado

---
+++ src/main/java/com/example/farmacia/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.example.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 2 , max = 100)
+	private String categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
